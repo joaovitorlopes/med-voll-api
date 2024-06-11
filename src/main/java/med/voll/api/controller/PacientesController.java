beee6f24ef111d6699512505b2d2c8dfb4e14927
joaovitorlopes <jvlaaa+github@gmail.com>
2024-06-10 23:09:18 -0400

    adding a feature of registering patients with migration to create patient table in database

--- src/main/java/med/voll/api/controller/PacientesController.java
+++ src/main/java/med/voll/api/controller/PacientesController.java
@@ -1,6 +1,11 @@
 package med.voll.api.controller;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.api.pacientes.DadosCadastroPacientes;
+import med.voll.api.pacientes.Paciente;
+import med.voll.api.pacientes.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("pacientes")
 public class PacientesController {
 
+    @Autowired
+    private PacienteRepository repository;
+
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroPacientes dados) {
-        System.out.println(dados);
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPacientes dados) {
+        repository.save(new Paciente(dados));
     }
 }
