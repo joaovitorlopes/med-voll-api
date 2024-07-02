c9710024fe8e8f88226c3bb90ef58253699ccaa1
joaovitorlopes <jvlaaa+github@gmail.com>
2024-07-02 12:12:07 -0400

    adding a feature of registering users with migration to create users table in database

---
+++ src/main/java/med/voll/api/domain/usuarios/Usuario.java
@@ -0,0 +1,35 @@
+package med.voll.api.domain.usuarios;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+    private String senha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+}
