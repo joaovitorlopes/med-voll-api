548a39903321b47c30f8e202ceb7a813fda0c46e
joaovitorlopes <jvlaaa+github@gmail.com>
2024-07-04 12:59:55 -0400

    adding a feature of test function to medico controller

---
+++ src/test/java/med/voll/api/controller/MedicosControllerTest.java
@@ -0,0 +1,100 @@
+package med.voll.api.controller;
+
+import med.voll.api.domain.enderecos.DadosEnderecos;
+import med.voll.api.domain.enderecos.Endereco;
+import med.voll.api.domain.medicos.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+class MedicosControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private JacksonTester<DadosCadastroMedicos> dadosCadastroMedicosJson;
+
+    @Autowired
+    private JacksonTester<DadosDetalhamentoMedico> dadosDetalhamentoMedicoJson;
+
+    @MockBean
+    private MedicoRepository medicoRepository;
+
+    @Test
+    @DisplayName("Deve devolver código http 400 quando informações estão inválidas")
+    @WithMockUser
+    void cadastrar_cenario1() throws Exception {
+        var response = mvc
+                .perform(post("/medicos"))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("Deve devolver código http 200 quando informações estão válidas")
+    @WithMockUser
+    void cadastrar_cenario2() throws Exception {
+        var dadosCadastro = new DadosCadastroMedicos(
+                "Medico",
+                "medico@voll.med",
+                "61999999999",
+                "123456",
+                Especialidade.CARDIOLOGIA,
+                dadosEndereco());
+
+        when(medicoRepository.save(any())).thenReturn(new Medico(dadosCadastro));
+
+        var response = mvc
+                .perform(post("/medicos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dadosCadastroMedicosJson.write(dadosCadastro).getJson()))
+                .andReturn().getResponse();
+
+        var dadosDetalhamento = new DadosDetalhamentoMedico(
+                null,
+                dadosCadastro.nome(),
+                dadosCadastro.email(),
+                dadosCadastro.crm(),
+                dadosCadastro.telefone(),
+                dadosCadastro.especialidade(),
+                new Endereco(dadosCadastro.endereco())
+        );
+        var jsonEsperado = dadosDetalhamentoMedicoJson.write(dadosDetalhamento).getJson();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
+    }
+
+    private DadosEnderecos dadosEndereco() {
+        return new DadosEnderecos(
+                "rua xpto",
+                "bairro",
+                "00000000",
+                "Brasilia",
+                "DF",
+                null,
+                null
+        );
+    }
+
+}
